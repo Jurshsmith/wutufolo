57c505616be955bfee1c8eaf1067da31320ee60c
Oladele Joshua <jurshsmith@gmail.com>
2020-08-02 13:48:37 -0400

    Backup this config

--- src/index.js
+++ src/index.js
@@ -2,15 +2,45 @@
 import { Octokit } from "@octokit/rest";
 import { range } from "rxjs";
 import { map, filter } from "rxjs/operators";
- 
-range(1, 200)
-  .pipe(
-    filter(x => x % 2 === 1),
-    map(x => x + x)
-  )
-  .subscribe(x => console.log(x));
+import { module } from "../webpack.config";
 
-const octokit = new Octokit();
+
+const getGitHubUsers = () => {
+
+    const octokit = new Octokit();
+    octokit.request('GET /users').then(value => console.log("Some value ", value));
+    
+}
+
+
+const simplePage = `
+    <button onclick="getGitHubUsers()">Click here to see logs</button>
+`;
+ 
+function mountApp(){
+
+    const app = document.querySelector('#app');
+    console.log('App ', app);
+
+    app.innerHTML = simplePage;
+
+}
+
+mountApp();
+// range(1, 200)
+//   .pipe(
+//     filter(x => x % 2 === 1),
+//     map(x => x + x)
+//   )
+//   .subscribe(x => console.log(x));
+
+module.exports = {
+    getGitHubUsers
+}
+
+
+
+
 
 
 // (async () => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-var HtmlwebpackPlugin = require('html-webpack-plugin');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -10,21 +11,39 @@ module.exports = {
   module: {
     rules: [
         {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              'style-loader',
+              // Translates CSS into CommonJS
+              'css-loader',
+              // Compiles Sass to CSS
+              'sass-loader',
+            ],
+        },
+        {
           test: /\.m?js$/,
           exclude: /(node_modules|bower_components)/,
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread']
             }
           }
         }
       ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlwebpackPlugin({
       title: 'Rxjs',
-      filename: 'index.html'
+      filename: 'index.html',
+      template: './src/public/index.html'
     }),
   ],
     // for the dev server
